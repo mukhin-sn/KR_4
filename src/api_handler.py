a6ebb5d315cde7ccffb1e26245cd8e5415d12c18
mukhin-sn <senikomu@gmail.com>
2023-11-13 01:35:29 +0700

    edit api_handler.py

--- src/api_handler.py
+++ src/api_handler.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
+import requests
+import os
 
 
 class APIHandler(ABC):
     """Абстрактный класс для работы с API сайтов"""
+
     @abstractmethod
     def api_handler(self):
         """Абстрактный метод для работы с API сайтов"""
@@ -11,17 +14,36 @@ class APIHandler(ABC):
 
 class SuperJobAPI(APIHandler):
     """Класс для работы с API сайта superjob.ru"""
-    def __init__(self):
-        pass
+
+    def __init__(self, **kwargs):
+        self.host = 'https://api.superjob.ru/2.0/vacancies/'
+        self.api_key = os.getenv('SJ_API_KEY')
+        self.params = kwargs
+        self.head = {"X-Api-App-Id": self.api_key}
+        self.resp = requests.get(self.host, headers=self.head, params=self.params).json()
+        # print(self.resp)
+
+    def __str__(self):
+        return self.api_key
 
     def api_handler(self):
         pass
 
 
-class HhRuAPI(APIHandler):
+class HHruAPI(APIHandler):
     """Класс для работы с API сайта hh.ru"""
-    def __init__(self):
-        pass
+
+    def __init__(self, **kwargs):
+        self.host = 'https://api.hh.ru/vacancies/'
+        self.params = kwargs
+        self.resp = requests.get(self.host, params=self.params).json()
+        # print(self.resp)
 
     def api_handler(self):
         pass
+
+
+#####################################################################################################################
+# obj_1 = SuperJobAPI()
+obj_2 = HHruAPI()
+# print(obj_1)
--- src/func.py
+++ src/func.py
@@ -1,25 +1,38 @@
 import requests
 import json
+import os
 
 
 url_var = 'https://api.hh.ru/vacancies/'
-response = requests.get(url_var, params={"page": 0, "per_page": 100})
+# url_var = 'https://api.superjob.ru/2.0/vacancies/'
+
+# param = {"Host": "api.superjob.ru",
+#         "X-Api-App-Id": os.getenv('SJ_API_KEY'),
+#         "Content-Type": "application/x-www-form-urlencoded"}
+
+response = requests.get(url_var, headers={"User-Agent": "python-requests/2.31.0"}, params={"page": 0, "per_page": 2}) #headers={"User-Agent": "MyApp/1.0"}
+# response = requests.get(url_var, headheaders={"User-Agent":={"X-Api-App-Id": os.getenv('SJ_API_KEY')})
+
+print(response.text)
+
 # data = response.content.decode()
 
-json_data = response.json()
+# json_data = response.json()
+# print(json_data)
+
 # print(type(json_data))
 # j_data = json.loads(json_data)
 # print(j_data)
 
-print(len(json_data["items"]))
-for i in range(len(json_data["items"])):
-    print(json_data["items"][i]["name"])
+# print(len(json_data["items"]))
+# for i in range(len(json_data["items"])):
+#     print(json_data["items"][i]["name"])
 # for key in json_data['name']:
 #     print(key)
 
 # print(len(json_data))
 # print(response.text)
-# print(response.request.headers)
+print(response.request.headers)
 
 # response = requests.get('https://httpbin.org/basic-auth/foo/bar')
 # print(response.status_code)  # 401
