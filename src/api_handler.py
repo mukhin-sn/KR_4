0627e50c422f292371e89d1b2a82f16584ff64c4
Sergey Mukhin <senikomu@gmail.com>
2023-11-18 21:17:04 +0700

    update project

--- src/api_handler.py
+++ src/api_handler.py
@@ -4,50 +4,100 @@ import os
 
 
 class APIHandler(ABC):
-    """Абстрактный класс для работы с API сайтов"""
+    """
+    Абстрактный класс для работы с API сайтов
+    """
 
     @abstractmethod
-    def api_handler(self):
-        """Абстрактный метод для работы с API сайтов"""
+    def api_handler(self, vacancy_name: str):
+        """
+        Абстрактный метод для работы с API сайтов
+        """
         pass
 
 
 class SuperJobAPI(APIHandler):
-    """Класс для работы с API сайта superjob.ru"""
+    """
+    Класс для работы с API сайта superjob.ru
+    """
 
-    def __init__(self, **kwargs):
+    def __init__(self):
         self.host = 'https://api.superjob.ru/2.0/vacancies/'
-        self.api_key = os.getenv('SJ_API_KEY')
-        self.params = kwargs
-        self.head = {"X-Api-App-Id": self.api_key}
-        self.resp = requests.get(self.host, headers=self.head, params=self.params)
-        self.resp_json = self.resp.json()["objects"][0]["profession"]
+        self.__api_key = os.getenv('SJ_API_KEY')
+        self.head = {"X-Api-App-Id": self.__api_key}
+        self.resp = requests.get(self.host, headers=self.head).request.headers
 
     def __str__(self):
-        out_dict = self.resp.request.headers
-        return ", ".join([f"{key}: {out_dict[key]}" for key in out_dict])
+        return ", ".join([f"{key}: {self.resp[key]}" for key in self.resp])
 
-    def api_handler(self):
-        pass
+    def api_handler(self, vacancy_name: str) -> list:
+        """
+        Метод получения списка вакансий, согласно фильтру
+        :param vacancy_name: - строка для фильтра
+        :return: -> list
+        """
+        out_list_vacancy = []
+        params = {
+            "count": 100,
+            "archive": False,
+            "keyword": vacancy_name
+            # "keywords[0][srws]": 1,
+            # "keywords[0][skwc]": "and",
+            # "keywords[0][keys]": keys,
+
+            # "keywords": {
+            #     "srws": 1,
+            #     "skwc": "and",
+            #     "keys": keys
+            #             }
+                  }
+        for num_page in range(5):
+            params["page"] = num_page
+            out_list_vacancy.extend(requests.get(self.host, headers=self.head, params=params).json()["objects"])
+        return out_list_vacancy
 
 
 class HHruAPI(APIHandler):
     """Класс для работы с API сайта hh.ru"""
 
-    def __init__(self, **kwargs):
+    def __init__(self,):
         self.host = 'https://api.hh.ru/vacancies/'
-        self.params = kwargs
-        self.resp = requests.get(self.host, params=self.params)
+        self.resp = requests.get(self.host).request.headers
 
     def __str__(self):
-        out_dict = self.resp.request.headers
-        return ", ".join([f"{key}: {out_dict[key]}" for key in out_dict])
+        return ", ".join([f"{key}: {self.resp[key]}" for key in self.resp])
 
-    def api_handler(self):
-        pass
+    def api_handler(self, vacancy_name: str) -> list:
+        """
+        Метод получения списка вакансий, согласно фильтру
+        :param vacancy_name: - строка для фильтра
+        :return: -> list
+        """
+        out_list_vacancy = []
+        params = {
+            "per_page": 100,
+            # "area": 1,
+            "text": vacancy_name
+                 }
+        for num_page in range(20):
+            params["page"] = num_page
+            out_list_vacancy.extend(requests.get(self.host, params=params).json()["items"])
+        return out_list_vacancy
 
 
 #####################################################################################################################
-obj_1 = SuperJobAPI(params={"keyword": "Python"})
-# obj_2 = HHruAPI()
-print(obj_1.resp)
+obj_1 = SuperJobAPI()
+obj_2 = HHruAPI()
+print(obj_1)
+print(obj_2)
+
+lst_vacancy = obj_2.api_handler("Python")
+print(len(lst_vacancy))
+# for i in lst_vacancy:
+#     print(i["profession"])
+# print(lst_vacancy[5])
+# print(len(lst_vacancy["objects"]))
+
+# for i in lst_vacancy:
+#     print(i["name"])
+# print(lst_vacancy[1])
--- src/func.py
+++ src/func.py
@@ -2,24 +2,28 @@ import requests
 import json
 import os
 
-
-# url_var = 'https://api.hh.ru/vacancies/'
-url_var = 'https://api.superjob.ru/2.0/vacancies/'
-
+country_id = 5
+url_var = 'https://api.hh.ru/areas/'  #'https://api.hh.ru/vacancies/'
+# url_var = 'https://api.superjob.ru/2.0/vacancies/'
+#
 # param = {"Host": "api.superjob.ru",
 #         "X-Api-App-Id": os.getenv('SJ_API_KEY'),
 #         "Content-Type": "application/x-www-form-urlencoded"}
 
 # response = requests.get(url_var, headers={"User-Agent": "python-requests/2.31.0"}, params={"page": 0, "per_page": 2}) #headers={"User-Agent": "MyApp/1.0"}
 
-response = requests.get(url_var, headers={"X-Api-App-Id": os.getenv('SJ_API_KEY')})
+# response = requests.get(url_var, headers={"X-Api-App-Id": os.getenv('SJ_API_KEY')})
+response = requests.get(url_var, params={"area": country_id})
+# print(os.getenv('SJ_API_KEY'))
 
 # print(response.text)
 
 # data = response.content.decode()
 
-# json_data = response.json()
-# print(json_data)
+json_data = response.json()
+# for i in json_data:
+#     print(i["areas"][0])
+# print(json_data["items"][10])
 
 # print(type(json_data))
 # j_data = json.loads(json_data)
@@ -33,11 +37,11 @@ response = requests.get(url_var, headers={"X-Api-App-Id": os.getenv('SJ_API_KEY'
 
 # print(len(json_data))
 # print(response.text)
-out_dict = response.request.headers
-print(out_dict)
+# out_dict = response.request.headers
+# print(out_dict)
 # out_str = ", ".join([key + ": " + out_dict[key] for key in out_dict])
-out_str = ", ".join([f"{key}: {out_dict[key]}" for key in out_dict])
-print(out_str)
+# out_str = ", ".join([f"{key}: {out_dict[key]}" for key in out_dict])
+# print(out_str)
 # print(response.request.headers)
 
 # response = requests.get('https://httpbin.org/basic-auth/foo/bar')
@@ -50,3 +54,32 @@ print(out_str)
 # for i in dict_test.items():
 #     print(f'{i[0]} - {i[1]}')
 #     print()
+
+
+def get_areas():
+    req = requests.get('https://api.hh.ru/areas')
+    data = req.content.decode()
+    req.close()
+    js_obj = json.loads(data)
+    areas = []
+    for k in js_obj:
+        for i in range(len(k['areas'])):
+            if len(k['areas'][i]['areas']) != 0:                      # Если у зоны есть внутренние зоны
+                for j in range(len(k['areas'][i]['areas'])):
+                    areas.append([k['id'],
+                                  k['name'],
+                                  k['areas'][i]['areas'][j]['id'],
+                                  k['areas'][i]['areas'][j]['name']])
+            else:                                                                # Если у зоны нет внутренних зон
+                areas.append([k['id'],
+                              k['name'],
+                              k['areas'][i]['id'],
+                              k['areas'][i]['name']])
+    return areas
+
+
+areas = get_areas()
+print(len(areas))
+# print(areas)
+for i in areas:
+    print(f"{i[0]} - {i[1]}, {i[2]} - {i[3]}")
